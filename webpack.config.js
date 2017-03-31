e77dbff40306861dba0a2767d4f1cca0f48e9a8a
Zetos Lima <cesar_manoel@hotmail.com>
2017-03-31 01:33:16 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index.js',
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'public'),
+        publicPath: '/'
+    },
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js']
+    },
+    stats: {
+        colors: true,
+        reasons: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    "babel-loader",
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(css|scss|sass)$/,
+                loader: 'style-loader!css-loader!sass-loader',
+            },
+        ],
+    },
+    devServer: {
+        hot: true,
+        contentBase: resolve(__dirname, 'public'),
+        publicPath: '/',
+        port: 3000,
+        compress: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+    ],
+};
