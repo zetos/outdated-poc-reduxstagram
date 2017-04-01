6ec86acfb7ad93be4320d70c6f1036c19864318e
Zetos Lima <cesar_manoel@hotmail.com>
2017-04-01 03:01:44 -0300

    Start of Snigle & PhotoGrid components

---
+++ src/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+    render() {
+        console.log('satanic');
+        return (
+            <div className="photo-grid">
+            Photo grid
+            </div>
+        );
+    },
+});
+
+export default PhotoGrid;
