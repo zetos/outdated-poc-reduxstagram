0863e4348c1e050a6d691245f52f49667587e23b
Zetos Lima <cesar_manoel@hotmail.com>
2017-04-04 16:54:57 -0300

    Store & start of redux implementation

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import {createStore, compse} from 'redux';
+// import {ConnectedRouter, routerReducer, routerMiddleware, push} from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+
+import rootReducer from './reduxers/index';
+
+// Data
+import comments from './data/comments';
+import posts from './data/posts';
+
+// Object with default data
+const defaultState = {
+    posts,
+    comments,
+};
+
+const store = createStore(rootReducer, defaultState);
+export const history = createHistory();
+export default store;
